9cb50d3cea196450d573edff379e6ed473e3b062
aretheregods <aretheregods@gmail.com>
2026-07-19 17:41:49 -0400

    Added more complete scaffolding for WNBAScraper

--- src/scrapers/wnba/wnba.mjs
+++ src/scrapers/wnba/wnba.mjs
@@ -1,13 +1,66 @@
 import { HTTPClient } from "#utils";
 
-export WNBAScraper extends HTTPClient {
+export class WNBAScraper extends HTTPClient {
 	constructor() {
-		super('https://stats.wnba.com');
+		super(
+			'https://stats.wnba.com/stats',
+			{
+				'referrer': 'https://stats.wnba.com',
+				'origin': 'https://stats.wnba.com',
+				'accept': 'application/json, text/plain, */*',
+				'accept-language': 'en-US,en;q=0.9',
+				'x-nba-stats-origin': 'stats',
+				'x-nba-stats-token': 'true'
+			}
+		);
 	}
 
-	async getSeasonGameSlugs(year) {}
+	async getSeasonGameSlugs(year) {
+        const url = `/leaguegamelog?Counter=0&Direction=DESC&LeagueID=10&PlayerOrTeam=T&Season=${ year }&SeasonType=02&Sorter=DATE`;
 
-	async getAPIBoxScore(gameId) {}
+		const data = await this.request(url, { headers: this.defaultHeaders } );
 
-	#mapResultSet(resultSet) {}
+		const rows = data.resultSets[0].rowSet;
+		const columns = data.resultSets[0].headers;
+
+		const gameIdIdx = columns.indexOf('GAME_ID');
+		const matchupIdx = columns.indexOf('MATCHUP');
+
+		const slugs = rows.map( row => {
+			const gameId = row[gameIdIdx];
+			const matchup = row[matchupIdx];
+			const cleanMatchup = matchup.toLowerCase().replace(/[\s\.]+/g, '').replace('@', '-vs-');
+			return `${ cleanMatchup }-${ gameId }`;
+		} );
+
+		return [...new Set(slugs)];
+	}
+
+	async getAPIBoxScore(gameId, type = 'traditional') {
+		const endpoint = `boxscore${ type }v2`;
+		const url = `${endpoint}?EndPeriod=10&EndRange=28800&GameID=${ gameId }&RangeType=0&StartPeriod=1&StartRange=0`;
+
+		const data = await this.request(url, { headers: this.defaultHeaders } );
+
+		const playerStatsSet = data.resultSets.find( set => set.name === 'PlayerStats' );
+		const teamStatsSet = data.resultSets.find( set => set.name === 'TeamStats' );
+
+		return {
+			players: this.#mapResultSet(playerStatsSet),
+			teams: this.#mapResultSet(teamStatsSet)
+		};
+	}
+
+	#mapResultSet(resultSet) {
+		if (!resultSet) return [];
+
+		const headers = resultSet.headers;
+		return resultsSet.rowSet.map( row => {
+			let obj = {};
+			row.forEach( (value, index) => {
+				obj[ headers[ index ] ] = value;
+			} );
+			return obj;
+		} );
+	}
 }
